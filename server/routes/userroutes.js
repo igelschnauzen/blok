a006a0a46365bb4ea58fb59a38788fadb9f5baa6
igelschnauzen <kriegg878@gmail.com>
2024-05-31 14:52:19 +0200

    router upd

--- server/routes/userroutes.js
+++ server/routes/userroutes.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {registerUser, loginUser, findUser, getUsers} = require("../controllers/userController");
 
+//CORS
+router.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
+
+router.options("/*", function(req, res, next){
+  console.log("options request! ");
+  res.send(200);
+});
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/find/:userId", findUser);
