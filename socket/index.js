06f10f86a2b8533f8ab7cb1e20d7391ae45105be
igelschnauzen <kriegg878@gmail.com>
2024-06-13 05:58:27 +0200

    ws

--- socket/index.js
+++ socket/index.js
@@ -3,8 +3,23 @@ const io = require("socket.io")(httpServer, {
   cors: "http://localhost:5173"
 });
 
+let onlineUsers = [];
+
 io.on("connection", (socket) => {
   console.log("New connection: " + socket.id);
+
+  socket.on("addNewUser", (userId) => {
+    if(!onlineUsers.some(user => user.userId === userId)) { //check if user is already online
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      }); 
+    }
+  });
+
+  console.log("onlineUsers", onlineUsers);
+
+  io.emit("getOnlineUsers", onlineUsers);
 });
 
-httpServer.listen(3000);
+io.listen(5000);
