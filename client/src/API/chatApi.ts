5102493e2811eb5b5c0216aad0ddf7648af6eac1
igelschnauzen <kriegg878@gmail.com>
2024-09-26 20:24:09 +0200

    nginx reverse proxy added

--- client/src/API/chatApi.ts
+++ client/src/API/chatApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const chatApi = createApi({
     reducerPath: "chatAPI",
-    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}:3000/api/chats` }),
+    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}/api/chats` }),
     endpoints: (build) => ({
         createChat: build.mutation({
             query: (createChatData) => ({
--- client/src/API/messageApi.ts
+++ client/src/API/messageApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const messageApi = createApi({
     reducerPath: "messageApi",
-    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}:3000/api/messages` }),
+    baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}/api/messages` }),
     endpoints: (build) => ({
         createMessage: build.mutation({
             query: (createMessageData) => ({
